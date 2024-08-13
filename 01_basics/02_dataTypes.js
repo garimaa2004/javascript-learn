0348b1868f80740c6d44be90d6753cde103678f2
garimaa2004 <148338965+garimaa2004@users.noreply.github.com>
2024-08-13 04:50:13 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+//alert (3+3) // we are using nodejs not browser
+console.log (3+3) 
+console.log("Garima")
+ console.log(3+
+    3)//code readablity should be high
+
+let name = "garima"
+let age = 20
+let isLoggedin = false
+let state;
+
+//number
+//bigint
+//string =>""
+//boolean =>true/false
+//null =>standalone value
+//undefined =>
+//symbol=>unique
+//object
+
+console.log (typeof null) //object
+console.log (typeof undefined)//undefined
